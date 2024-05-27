aa1d1f13c2da744a4fefc2073735e7e079ca6093
Francesco Trevisan <FRANCESCO.TREVISAN@studenti.units.it>
2024-05-27 18:01:40 +0200

    Final Commit

--- ZhangCalibration.py
+++ ZhangCalibration.py
@@ -40,7 +40,6 @@ def estimateHomography(corners, real_coordinates):
     h =Vtransposed.transpose()[:,-1] #ci serve V trasposta e solo l'ultima colonna
     H = h.reshape(3,3) #da un array 9 a un 3x3
     #H è la nostra omografia!
-    print(H)
     return H
 
 def findVijValue(H,i,j): #calcoliamo il valore vij di V
@@ -64,21 +63,17 @@ grid_size = (8,11)
 homographies = []
 for p in images_path:
     corners = []
-    #trova corners (faccio prima senza refinement, dopo con)
-    print(p)
     im = cv2.imread(p)
     return_value, corners = cv2.findChessboardCorners(im, patternSize=grid_size, corners=None)
     if not return_value:
         print(f"pattern not found for image {p}")
     corners=corners.reshape((88,2)).copy()
-    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 100, 0.001) # tuple for specifying the termination criteria of the iterative refinement procedure cornerSubPix()
-    corners = cv2.cornerSubPix(gray,corners,(5,5),(-1,-1),criteria)
+    #gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    #criteria = (cv2.TERM_CRITERIA_MAX_ITER | cv2.TERM_CRITERIA_EPS, 100, 0.001) # tuple for specifying the termination criteria of the iterative refinement procedure cornerSubPix()
+    #corners = cv2.cornerSubPix(gray,corners,(5,5),(-1,-1),criteria)
     real_coordinates = findRealCoordinates(corners)
     H = estimateHomography(corners, real_coordinates)
     homographies.append(H)
-
-print(homographies)
 #adesso che abbiamo l'omografia H, dobbiamo trovare P
 #per farlo seguiamo la procedura di zhang con Vb=0
 
@@ -91,7 +86,6 @@ for h in homographies:
     v11= findVijValue(h,0,0)
     v22= findVijValue(h,1,1)
     v.append(np.subtract(v11,v22))
-    print(v)
     V.append(v)
 
 #Scomposizione UESt della matrice V
@@ -101,7 +95,6 @@ U,E,Stransposed = np.linalg.svd(V)
 S = Stransposed.transpose()
 #la sol di Vb=0 è l'ultima colonna di S
 b = S[:,-1]
-print(S)
 B = np.empty((3,3))
 B[0]= [b[0],b[1],b[3]]
 B[1]= [b[1],b[2],b[4]]
@@ -115,47 +108,43 @@ if not is_pos_def(B):
 
 L= np.linalg.cholesky(B) #lower triangular
 Lt = L.transpose()
-print(L)
-print(Lt)
 
 #La calibration matrix è l'inversa della trasposta
 
 K = np.linalg.inv(Lt)
 #K(3,3) viene leggermente sopra 1, quindi lo settiamo a 1
+print("K=",K)
 K[2][2] = 1
-print(K)
 #adesso calcoliamo r e t per ogni immagine
 rtMatrix = []
 count=0
 for h in homographies:
     print(h)
-    h1 = h[:,0]
-    h2 = h[:,1]
-    h3 = h[:,2]
-    denonim = np.linalg.norm(np.linalg.inv(K)@h1)
+    h1 = np.array(h[:,0])
+    h2 = np.array(h[:,1])
+    h3 = np.array(h[:,2])
+    denonim = np.linalg.norm(np.matmul(np.linalg.inv(K),h1.transpose()))
     lambd = 1/denonim
-    r1 = lambd *(np.linalg.inv(K)@h1)
-    r2 = lambd *(np.linalg.inv(K)@h2)
+    r1 = lambd *(np.matmul(np.linalg.inv(K),h1.transpose()))
+    r2 = lambd *(np.matmul(np.linalg.inv(K),h2.transpose()))
     r3 = np.cross(r1,r2)
-    t = r1 = lambd *(np.linalg.inv(K)@h3)
+    t = lambd *(np.linalg.inv(K)@h3)
     rtMatrix.append([r1,r2,r3,t])
-    print(rtMatrix[count])
     count+=1
 rtMatrix= np.array(rtMatrix)
-print(rtMatrix)
 
 #reprojection error, prendiamo l'immagine image03.tiff
 totRepErr= 0
-immagine = cv2.imread(folderpath + "image03.tiff")
-paramIntrinsechi = np.array(rtMatrix[3]).transpose()
-print(rtMatrix[3])
-print(rtMatrix[3].transpose())
+immagine = cv2.imread(folderpath + "image05.tiff")
+paramIntrinsechi = np.array(rtMatrix[5]).transpose()
 P = K@paramIntrinsechi
 return_value, puntipixel = cv2.findChessboardCorners(immagine, patternSize=grid_size, corners=None)
 puntipixel= puntipixel.reshape((88,2)).copy()
 coordReali =  findRealCoordinates(puntipixel)
-print(coordReali)
-for coord in coordReali:
+print(puntipixel)
+#for pixel in puntipixel:
+    #immagine = cv2.circle(immagine, (int(pixel[0]),int(pixel[1])), radius=3, color=(0, 255, 0), thickness=1)
+for i, coord in enumerate(coordReali):
     #troviamo le coordinate omogenee
     homog = np.array([coord[0],coord[1],0,1])
     #proiettiamo
@@ -163,9 +152,51 @@ for coord in coordReali:
     projections = np.empty(2)
     projections[0]= int(project_hom[0]/project_hom[2])
     projections[1] = int(project_hom[1]/project_hom[2])
-    immagine = cv2.circle(immagine, (int(projections[0]),int(projections[1])), radius=4, color=(255, 0, 0), thickness=1)
+    immagine = cv2.circle(immagine, (int(projections[0]),int(projections[1])), radius=7, color=(255, 0, 0), thickness=1)
+    immagine = cv2.circle(immagine, (int(puntipixel[i][0]),int(puntipixel[i][1])), radius=7, color=(0, 255, 0), thickness=1)
+    totRepErr += (projections[0]-puntipixel[i][0])**2 + (projections[1]-puntipixel[i][1])**2
+print(totRepErr)
 plt.imshow(immagine)
 plt.show()
+
+#superimposing di un rettangolo
+rect_width = 99
+rect_height = 44
+
+bottom_left_corner_x = -11
+bottom_left_corner_y = 33
+superimposed = []
+for i,im in enumerate(images_path):
+    #base rettangolo
+    image = cv2.imread(im)
+    overlay = image.copy()
+    vx = bottom_left_corner_x + np.array([0, 0, rect_width, rect_width])
+    vy = bottom_left_corner_y + np.array([0, rect_height, rect_height, 0])
+    homogeneousIN = np.vstack((vx, vy, np.ones_like(vx)))
+    homogeneousOUT = homographies[i] @ homogeneousIN #moltiplicazione tra mappa e input
+    xyOUT = (homogeneousOUT/homogeneousOUT[2])[:2] #in questo caso stiamo dividendo per la terza variabile e teniamo le prime 2
+    cv2.polylines(image,np.int32([xyOUT.transpose()]), isClosed=True, color=(0,0,0),thickness=4)
+    cv2.fillPoly(overlay,np.int32([xyOUT.transpose()]),color=(255,0,0))
+    #parte rialzata
+    height = 25
+    homogeneousIN2 = np.vstack((vx, vy, np.full_like(vx,height),np.ones_like(vx)))
+    paramIntrinsechi = np.array(rtMatrix[i])
+    P = K@paramIntrinsechi.transpose()
+    homogeneousOUT2 = P @homogeneousIN2
+    xyzOUT = (homogeneousOUT2/homogeneousOUT2[2])[:2]
+    cv2.polylines(image,np.int32([xyzOUT.transpose()]), isClosed=True, color=(0,0,0),thickness=4)
+    cv2.fillPoly(overlay,np.int32([xyzOUT.transpose()]), color=(0,255,0))
+    image_new = cv2.addWeighted(overlay, 0.4, image, 1 - 0.4, 0)
+    superimposed.append(image_new)
+
+
+
+
+compound_image=create_compound_image(rows=4,cols=5,limages=superimposed)
+plt.figure(figsize=(20, 20))
+plt.imshow(compound_image)
+plt.show()
+
 
 
 
